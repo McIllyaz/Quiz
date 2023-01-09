924bad955e804847bd349f639b24d7d0a444c941
Illyaz Arya <71999096+McIllyaz@users.noreply.github.com>
2023-01-10 00:31:04 +0700

    Update debug.cpp

--- debug.cpp
+++ debug.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
-#include<conio.h>
+#include <iostream>
+#include <conio.h>
 #include <cstring>
 #include <algorithm>
 #include <ctime>
@@ -7,6 +7,9 @@ using namespace std;
 #define compareBoxes(box1, box2, box3) ((board[box1] == board[box2]) && (board[box2] == board[box3]) && (board[box1] != 0)) //Checkes if three items are the same, and makes sure they're not 0's.
 #define numberToLetter(x) ((x > 0) ? (x == 1) ? 'X' : 'O' : ' ') //Takes the number and turns it into the letter or space.
 
+string nama, prodi;
+char yt;
+int plh;
 int getWinner(int board[9]) {
 	//Finds winner of game, if there is no winner, returns 0.
 	int winner = 0;
@@ -109,11 +112,117 @@ void outputBoard(int board[9]) {
 		cout << ((line < 2) ? "-----\n" : "\n");
 	}
 }
+
+int soal2(){
+   
+    string questions_dua[5] = {
+    "Dibawah ini yang merupakan bagian dari sistem operasi, kecuali",
+    "Perangkat yang terdapat dalam sistem komputer diantaranya adalah",
+    "Fungsi alat ini ialah untuk memindai data atau dokumen dengan cara menduplikasi dalam bentuk digital atau file dalam komputer. Alat ini termasuk jenis hardware",
+    "Perangkat lunak uji coba yang diberikan secara gratis dengan keterbatasan fitur-fitur tertentu seperti ketersediaan, fungsi, dan kenyaman yang tidak dapat dimanfaatkan secara maksimal ialah",
+    "Kelompok 2: Hardisk, flash disk, CD/DVD, SSD Kelompok 3: Monitor, printer, speaker, plotter Kelompok 4: RAM, processor, VGA card, \nmainboard Yang termasuk ke dalam perangkat keras keluaran adalah", };
+
+    string option_dua[5][4] = { 
+        {"Kelompok 1", "Kelompok 2", "Kelompok 3", "Kelompok 4"},
+        {"Commercial Software", "Shareware", "Freeware", "Openware"},
+        {"Input device", "Output device", "Storage device", "Processing device"},
+        {"Hardware, program dan CPU", "Software, Brainware dan Linux", "User, program dan perangkat lunak", "Hardware, software dan brainware"},
+        {"CPU", "Linux", "Mac OS", "DOS"}
+        };
+
+    string correct_ans_dua[5]{
+        "Kelompok 3", "Freeware", "Processing device", "Hardware, software dan brainware", "CPU",
+    };
+    int selected_option_dua[5] = {0, 0, 0, 0, 0};
+	int totalQ = 5;
+	int op;
+
+    soal2: 
+    cout << "\n====================================================================================\n";
+    cout << "Apakah anda ingin mengerjakan soal sekarang? (y/t)\n";
+    cin >> yt;
+    cout << "====================================================================================\n";
+
+
+    if (yt == 't' || yt == 'T')
+    {
+        exit(0);
+	}else if (yt == 'y' || yt == 'Y'){
+        for(int j = 0; j < totalQ; j++){
+            cout << "Question No: " << (j+1) << endl;
+            cout << questions_dua[j] << endl;
+            cout << "1." <<  option_dua[j][0] << endl;
+            cout << "2." <<  option_dua[j][1] << endl;
+            cout << "3." <<  option_dua[j][2] << endl;
+            cout << "4." <<  option_dua[j][3] << endl << endl;
+
+            cout << "0 is for skip. Select your answer as 1, 2, 3 or 4 here : ";
+            cin >> selected_option_dua[j];
+            cout << endl << "==========================================================" << endl << endl;
+        }
+
+        system("cls");
+        cout << "********************************* " << endl;
+        cout << "****** Compare your answer ****** " << endl;
+        cout << "********************************* " << endl << endl;
+
+        for(int j = 0; j < totalQ; j++){
+            cout << "Question No: " << (j+1) << endl;
+            cout << questions_dua[j] << endl;
+            cout << "1." << option_dua[j][0] << endl;
+            cout << "2." << option_dua[j][1] << endl;
+            cout << "3." << option_dua[j][2] << endl;
+            cout << "4." << option_dua[j][3] << endl;
+
+            if( selected_option_dua[j] == 0 ){
+                cout << "You have Skipped this Question." << endl;
+            }else{
+                cout << "You have Selected\t: " << option_dua[j][ selected_option_dua[j]-1] << endl;
+            }
+            cout << "Correct Option was\t: " << correct_ans_dua[j] << endl << endl;
+
+            cout << "Press any key to continue checking..." << endl;
+            getch();
+            cout << endl << "=============================================" << endl;
+        }
+        cout << endl << endl;
+        cout << "************************************ " << endl;
+        cout << "*********** Your Result ************ " << endl;
+        cout << "************************************ " << endl << endl;
+
+        int correct = 0;
+        int incorrect = 0;
+        int skipped = 0;
+
+        for(int j = 0; j < totalQ; j++){
+            if(selected_option_dua[j] != 0){
+                if(correct_ans_dua[j] == option_dua[j][selected_option_dua[j]-1]){
+                    correct++;
+                }else{
+                    incorrect++;
+                }
+            }else{
+                skipped++;
+            }
+        }
+        cout << "Nama\t\t\t: " << nama << endl;
+        cout << "Prodi\t\t\t: " << prodi << endl;
+        cout << "Correct answer\t\t: " << correct << endl;
+        cout << "Wrong answer\t\t: " << incorrect << endl;
+        cout << "Skipped\t\t\t: " << skipped << endl;
+
+        
+    }else{
+        system("cls");
+        cout << "Maaf inputan tidak valid :(" << endl;
+        goto soal2;
+    }
+}
+/////////
 int main(){
-    char yt;
 
     string all_questions[10] = {    // all the questions
-	"Diketahui bahwa kantong P kosong, kantong Q berisi 10 buah kelereng dan kantong R berisi 15 kelereng.\nApabila yang terbawa hanya sebuah kantong dan dikatakan BUKAN Kantong P yang terbawa\n maka jumlah kelereng yang terbawa adalah :",
+	"Diketahui bahwa kantong P kosong, kantong Q berisi 10 buah kelereng dan kantong R berisi 15 kelereng.\nApabila yang terbawa hanya sebuah kantong dan dikatakan BUKAN Kantong P yang terbawa\nmaka jumlah kelereng yang terbawa adalah :",
 	"Diberikan algoritma : \nApabila warna merah maka jadi hijau., \nApabila warna hijau maka jadi putih, \nselain warna merah dan hijau maka jadi ungu. \nJika kondisi input warna adalah hitam, maka warna jadi :",
 	"Instruksi P=Q akan mengakibatkan nilai P = nilai Q, dan nilai Q menjadi :",
 	"Apabila a=5, b = 10, maka jika diberikan instruksi a=b; b=a akan mengakibatkan ",
@@ -147,90 +256,126 @@ int main(){
 	int totalQ = 10;
 	int op;
 
-    cout << "\n====================================================================================\n";
-    cout << "Selamat datang di aplikasi quiz:)\nAplikasi ini berisi 30 soal terdiri dari 3 mata kuliah dengan masing-masing soal 10.\nApakah anda ingin mengerjakan soal? (y/t)\n";
+
+    pilih:
+
+
+    cout << "========================================================\n";
+    cout << "Selamat datang di aplikasi quiz:)\nAplikasi ini berisi 10 soal terdiri dari 1 mata kuliah.\nApakah anda ingin mengerjakan soal? (y/t)\n";
     cin >> yt;
-    cout << "====================================================================================\n";
+    cout << "========================================================\n";
 
-repetisi:
-    if (yt =='t' || yt =='T')
-    {
+
+    if (yt =='t' || yt =='T'){
         exit(0);
-	}
+	}else if(yt =='y' || yt =='Y'){
+        cout << "Nama : ";
+        cin >> nama;
+        cout << "Prodi: ";
+        cin >> prodi;
+        repetisi:
+        for(int i = 0; i < totalQ; i++){
+            cout << "Question No: " << (i+1) << endl;
+            cout << all_questions[i] << endl;
+            cout << "1." << all_options[i][0] << endl;
+            cout << "2."<<all_options[i][1] << endl;
+            cout << "3." << all_options[i][2] << endl;
+            cout << "4." << all_options[i][3] << endl << endl;
+
+            cout << "0 is for skip. Select your answer as 1, 2, 3 or 4 here : ";
+            cin >> selected_option[i];
+            cout << endl << "==========================================================" << endl << endl;
+        }
+
+        //----- Printing Correct Options -----
+        system("cls");
+        cout << "********************************* " << endl;
+        cout << "****** Compare your answer ****** " << endl;
+        cout << "********************************* " << endl << endl;
+
+        for(int i = 0; i < totalQ; i++){
+            cout << "Question No: " << (i+1) << endl;
+            cout << all_questions[i] << endl;
+            cout << "1." << all_options[i][0] << endl;
+            cout << "2." << all_options[i][1] << endl;
+            cout << "3."<< all_options[i][2] << endl;
+            cout << "4." << all_options[i][3] << endl;
+
+            if(selected_option[i] == 0 ){
+                cout << "You have Skipped this Question." << endl;
+            }else{
+                cout << "You have Selected\t: " << all_options[i][selected_option[i]-1] << endl;
+            }
+            cout << "Correct Option was\t: " << correct_ans[i] << endl << endl;
+
+            cout << "Press any key to continue checking..." << endl;
+            getch();
+            cout << endl << "=============================================" << endl;
+        }
+
+        //----- Printing Result -----
+        cout << endl << endl;
+        cout << "************************************ " << endl;
+        cout << "*********** Your Result ************ " << endl;
+        cout << "************************************ " << endl << endl;
+
+        int correct = 0;
+        int incorrect = 0;
+        int skipped = 0;
+
+        for(int i = 0; i < totalQ; i++){
+            if(selected_option[i] != 0){
+                if(correct_ans[i] == all_options[i][selected_option[i]-1]){
+                    correct++;
+                }else{
+                    incorrect++;
+                }
+            }else{
+                skipped++;
+            }
+        }
+        cout << "Nama\t\t\t: " << nama << endl;
+        cout << "Prodi\t\t\t: " << prodi << endl;
+        cout << "Correct answer\t\t: " << correct << endl;
+        cout << "Wrong answer\t\t: " << incorrect << endl;
+        cout << "Skipped\t\t\t: " << skipped << endl;
+        cout << "=======================================" << endl;
+    }else{
+        system("cls");
+        cout << "Maaf inputan tidak valid :(" << endl;
+        goto pilih;
+    }
     
-    for(int i = 0; i < totalQ; i++){
-		cout << "Question No: " << (i+1) << endl;
-		cout << all_questions[i] << endl;
-		cout << "1." << all_options[i][0] << endl;
-		cout << "2."<<all_options[i][1] << endl;
-		cout << "3." << all_options[i][2] << endl;
-		cout << "4." << all_options[i][3] << endl << endl;
-
-		cout << "0 is for skip. Select your answer as 1, 2, 3 or 4 here : ";
-		cin >> selected_option[i];
-		cout << endl << "==========================================================" << endl << endl;
-	}
-
-	//----- Printing Correct Options -----
-    system("cls");
-	cout << "********************************* " << endl;
-	cout << "****** Compare your answer ****** " << endl;
-	cout << "********************************* " << endl << endl;
-
-	for(int i = 0; i < totalQ; i++){
-		cout << "Question No: " << (i+1) << endl;
-		cout << all_questions[i] << endl;
-		cout << "1." << all_options[i][0] << endl;
-		cout << "2." << all_options[i][1] << endl;
-		cout << "3."<< all_options[i][2] << endl;
-		cout << "4." << all_options[i][3] << endl;
-
-		if(selected_option[i] == 0 ){
-			cout << "You have Skipped this Question." << endl;
-		}else{
-			cout << "You have Selected\t: " << all_options[i][selected_option[i]-1] << endl;
-		}
-		cout << "Correct Option was\t: " << correct_ans[i] << endl << endl;
-
-		cout << "Press any key to continue checking..." << endl;
-		getch();
-		cout << endl << "=============================================" << endl;
-	}
-
-	//----- Printing Result -----
-	cout << endl << endl;
-	cout << "************************************ " << endl;
-	cout << "*********** Your Result ************ " << endl;
-	cout << "************************************ " << endl << endl;
-
-	int correct = 0;
-	int incorrect = 0;
-	int skipped = 0;
-
-	for(int i = 0; i < totalQ; i++){
-		if(selected_option[i] != 0){
-			if(correct_ans[i] == all_options[i][selected_option[i]-1]){
-				correct++;
-			}else{
-				incorrect++;
-			}
-		}else{
-			skipped++;
-		}
-	}
-	cout << "Total questions\t: " << totalQ << endl;
-	cout << "Correct answer\t\t: " << correct << endl;
-	cout << "Wrong answer\t\t: " << incorrect << endl;
-	cout << "Skipped\t\t\t: " << skipped << endl;
-	cout << "=======================================" << endl;
-	cout << "Apakah anda ingin mencoba lagi? (y/t)" << endl;
-	cin >> yt;
-	if (yt =='y' || yt =='Y')
+    lagi:
+	cout << "Apakah anda ingin mencoba lagi? (1. ulang) (2. soal lain) (3. main game)" << endl;
+	cin >> plh;
+	if (plh == 1)
 	{
 		system("cls");
 		goto repetisi;
-	}else
-	{ 
+    }
+    else if(plh == 2)
+    {
+       system("cls");
+       soal2();
+        pilih_lagi :
+        cout << "=========================================================" << endl;   
+        cout << "Apakah anda ingin mencoba lagi? (1. ulang) (2. main game)" << endl;
+	    cin >> plh;
+        if (plh == 1){
+            system("cls");
+            soal2();
+            goto pilih_lagi;
+        }else if (plh == 2){
+            goto game;
+        }else{
+            system("cls");
+            cout << "Maaf Inputan tidak valid :(\n";
+            cout << "==============================";
+            goto pilih_lagi;
+        }
+	}
+    else if (plh == 3){ 
 	game: 
 	int board[9] = {0,0,0,0,0,0,0,0,0}; //Starts empty board.
 	int possibleWinner;
@@ -289,7 +434,7 @@ repetisi:
 		if(gameOver(board)) break;
 
 	}
-	cout << "Apakah Anda ingin main lagi? (y/t)";
+	cout << "Apakah Anda ingin main lagi? (y/t): \n";
 	cin >> yt;
 	if (yt =='y' || yt == 'Y')
 	{
@@ -297,10 +442,37 @@ repetisi:
 		goto game;
 	}else if(yt =='t' || yt == 'T')
 	{
-		cout << "Demikian presentasi dari kami";
+		cout << "\n\t     Demikian presentasi dari kami\n";
+        cout << "\t........................................."<< endl;
+        cout << "\t:::::: 000::::::::::::::::::::000::::::::"<< endl;
+        cout << "\t:::::: 0000:::::::::::::::::::0000:::::::"<< endl;
+        cout << "\t::::::00000::::::::::::::::::000000::::::"<< endl;
+        cout << "\t::::::000000:::::::::::::::::000000::::::"<< endl;
+        cout << "\t::::::000000:::::::::::::::::000000::::::"<< endl;
+        cout << "\t::::::000000:::::::::::::::::000000::::::"<< endl;
+        cout << "\t::::::000000:::0000:::0000:::000000::::::"<< endl;
+        cout << "\t::::::000000::000000:000000::000000::::::"<< endl;
+        cout << "\t::::::000000::000000:000000::000000::::::"<< endl;
+        cout << "\t::::::000000::000000:000000::000000::::::"<< endl;
+        cout << "\t::::::000000::000000:000000::000000::::::"<< endl;
+        cout << "\t::::::000...::......:......::....00......"<< endl;
+        cout << "\t::::::000....ooooooooooo'8888 ...00::::::"<< endl;
+        cout << "\t::::::00....ooooooooooo'888888...00::::::"<< endl;
+        cout << "\t::::::00...ooooooo-oooo '8888....00::::::"<< endl;
+        cout << "\t::::::00**oooooo.................00......"<< endl;
+        cout << "\t::::::00*oooooo*...............0000......"<< endl;
+        cout << "\t::::::00oooooo*...............0000......."<< endl;
+        cout << "\t.......0000000...............0000........"<< endl;
+        cout << "\t........00000...............00000........"<< endl;
+        cout << "\t.........00000.............00000........."<< endl;
+        cout << "\t..........0000............00000.........."<< endl;
+        cout << "\t...........0000000......00000............"<< endl;
+        cout << "\t.................00000000................"<< endl;
 		exit(0);
 	}
-	}
+	}else{
+        cout << "inputan tidak valid\n";
+    }
 	getch();
 	return 0;
 }
